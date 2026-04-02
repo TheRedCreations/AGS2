702cf9a03f4aefe5498307964664e01fa0155fbc
TheRedCreations <35812887+TheRedCreations@users.noreply.github.com>
2026-04-02 14:26:33 +0200

    Fix filename

--- 0/alert.lua
+++ 0/alert.lua
@@ -1,6 +1,6 @@
 while true do
   if redstone.getInput("top") == true or redstone.getInput("bottom") == true or redstone.getInput("left") == true or redstone.getInput("right") == true or redstone.getInput("front") == true or redstone.getInput("back") == true then
-shell.run("test.lua")
+shell.run("speakers.lua")
   sleep(2)
   end
   sleep(0.1)
